632bf846c5043305b51287b1ac0d018671f9129e
Wanru <48364194+WanruXX@users.noreply.github.com>
2024-05-07 19:47:23 +0000

    finish 6.4

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
 import "./Description.css";
 
-const Description = ({ weatherDescription }) => {
+const Description = ({ isLoading, weatherDescription }) => {
   return (
     <div className="description">
       <h2 className="description__title">Description</h2>
       <div className="description__divider">
+        {isLoading && <Loader />}
         <p className="description__text">{weatherDescription}</p>
       </div>
     </div>
